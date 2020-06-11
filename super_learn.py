a67bf9a787f329c2819ba5ba34e9c8250509cc22
Velda Kiara <veldakarimi@gmail.com>
2020-06-12 00:06:49 +0300

    differentiating positive and negative texts

---
+++ super_learn.py
@@ -0,0 +1,22 @@
+from texts import text_counter, text_training
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+
+intercepted_text = "I love Software Development."
+
+text_counts = text_counter.transform([intercepted_text])
+
+text_classifier = MultinomialNB()
+
+text_labels = [0] * 1000 + [1] * 1000
+
+text_classifier.fit(text_training, text_labels)
+
+final_pos = text_classifier.predict_proba(text_counts)[0][1]
+
+final_neg = text_classifier.predict_proba(text_counts)[0][0]
+
+if final_pos > final_neg:
+  print("The text is positive.")
+else:
+  print("The text is negative.")
