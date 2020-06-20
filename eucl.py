7af5e15406f9bc7be163d37aa3d2a8b4426ca898
Velda Kiara <veldakarimi@gmail.com>
2020-06-20 03:43:09 +0300

    addition of for loop and if

--- eucl.py
+++ eucl.py
@@ -17,4 +17,7 @@ print(manhattan_distance([5, 4, 3], [1, 7, 9]))
 
 def hamming_distance(pt1,pt2):
     distance = 0
+    for i in range(len(pt1)):
+        if pt1[i] != pt2[i]:
+            distance += 1
     return distance
