d404aac389776ae234c47de8415cd566b5ec5cda
Velda Kiara <veldakarimi@gmail.com>
2020-06-20 03:00:48 +0300

    using euclean distance

--- eucl.py
+++ eucl.py
@@ -2,4 +2,7 @@ def euclidean_distance(pt1, pt2):
     distance = 0
     for i in range(len(pt1)):
         distance += (pt1[i] - pt2[i]) ** 2
-    return distance
+    return distance ** 0.5
+
+print(euclidean_distance([1, 2], [4, 0]))
+print(euclidean_distance([5, 4, 3], [1, 7, 9]))
