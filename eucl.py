cf63792da1a1055aa88b58b375b8d446b8bbedda
Velda Kiara <veldakarimi@gmail.com>
2020-06-20 03:33:09 +0300

    completed manhattan distance

--- eucl.py
+++ eucl.py
@@ -12,3 +12,5 @@ def manhattan_distance(pt1, pt2):
     for i in range(len(pt1)):
         distance += abs(pt1[i] - pt2[i]) 
     return distance
+print(manhattan_distance([1, 2], [4, 0]))
+print(manhattan_distance([5, 4, 3], [1, 7, 9]))
