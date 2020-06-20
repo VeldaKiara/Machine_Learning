dbfbd446953faddfa272de7af4a98eb86bde560d
Velda Kiara <veldakarimi@gmail.com>
2020-06-20 03:44:14 +0300

    completion of hamming distance

--- eucl.py
+++ eucl.py
@@ -21,3 +21,5 @@ def hamming_distance(pt1,pt2):
         if pt1[i] != pt2[i]:
             distance += 1
     return distance
+print(hamming_distance([1, 2], [1, 100]))
+print(hamming_distance([5, 4, 9], [1, 7, 9]))
